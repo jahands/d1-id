ee621ed10242e8ac73df88b2089d22c23f0ed7eb
Jacob Hands <jacob@gogit.io>
2022-06-18 13:36:00 -0500

    Formatting

--- src/api/namespaces.ts
+++ src/api/namespaces.ts
@@ -2,7 +2,13 @@ import { Database } from "@cloudflare/d1";
 import { getDB } from "./db";
 import { IttyRequest, Env } from "./types";
 import users from "./users";
-import { alreadyExists, hasResults, missingParams, notExists, now } from "./util";
+import {
+  alreadyExists,
+  hasResults,
+  missingParams,
+  notExists,
+  now,
+} from "./util";
 
 export type Namespace = {
   namespace_id: number;
@@ -28,75 +34,85 @@ export async function getNamespaceID(
   return null;
 }
 
-async function getNamespaces(req: IttyRequest, env: Env, _ctx: ExecutionContext) {
-    if (!req.params) {
-        return missingParams();
-    }
-    const db = getDB(env);
-    // get the user
-    const userID = await users.getUserID(db, req.params.user);
-    if (!userID) {
-        return notExists("user");
-    }
+async function getNamespaces(
+  req: IttyRequest,
+  env: Env,
+  _ctx: ExecutionContext
+) {
+  if (!req.params) {
+    return missingParams();
+  }
+  const db = getDB(env);
+  // get the user
+  const userID = await users.getUserID(db, req.params.user);
+  if (!userID) {
+    return notExists("user");
+  }
 
-    const namespaces = await db
-        .prepare("SELECT * FROM namespaces WHERE user_id = ?")
-        .bind(userID)
-        .all();
-    return Response.json(namespaces.results || []);
+  const namespaces = await db
+    .prepare("SELECT * FROM namespaces WHERE user_id = ?")
+    .bind(userID)
+    .all();
+  return Response.json(namespaces.results || []);
 }
 
-async function createNamespace(req: IttyRequest, env: Env, _ctx: ExecutionContext) {
-    if (!req.params) {
-        return missingParams();
-    }
-    const db = getDB(env);
+async function createNamespace(
+  req: IttyRequest,
+  env: Env,
+  _ctx: ExecutionContext
+) {
+  if (!req.params) {
+    return missingParams();
+  }
+  const db = getDB(env);
 
-    // get the user
-    const userID = await users.getUserID(db, req.params.user);
-    if (!userID) {
-        return notExists("user");
-    }
+  // get the user
+  const userID = await users.getUserID(db, req.params.user);
+  if (!userID) {
+    return notExists("user");
+  }
 
-    // Check if the namespace already exists
-    const namespaceID = await getNamespaceID(db, userID, req.params.namespace);
-    if (namespaceID) {
-        return alreadyExists("namespace");
-    }
-    const res = await db
-        .prepare(
-            "INSERT INTO namespaces (user_id,name,created_on) VALUES (?,?,?)"
-        )
-        .bind(userID, req.params.namespace, now())
-        .run();
-    return Response.json(res);
+  // Check if the namespace already exists
+  const namespaceID = await getNamespaceID(db, userID, req.params.namespace);
+  if (namespaceID) {
+    return alreadyExists("namespace");
+  }
+  const res = await db
+    .prepare("INSERT INTO namespaces (user_id,name,created_on) VALUES (?,?,?)")
+    .bind(userID, req.params.namespace, now())
+    .run();
+  return Response.json(res);
 }
 
-async function deleteNamespace(req: IttyRequest, env: Env, _ctx: ExecutionContext) {
-    if (!req.params) {
-        return missingParams();
-    }
-    const db = getDB(env);
-    // get the user
-    const userID = await users.getUserID(db, req.params.user);
-    if (!userID) {
-        return notExists("user");
-    }
-    // Check if the namespace exists
-    const namespaceID = await getNamespaceID(db, userID, req.params.namespace);
-    if (!namespaceID) {
-        return notExists("namespace");
-    }
-    // Delete the namespace
-    const res = await db
-        .prepare("DELETE FROM namespaces WHERE user_id = ? AND name = ?")
-        .bind(userID, req.params.namespace)
-        .run();
-    return Response.json(res);
+async function deleteNamespace(
+  req: IttyRequest,
+  env: Env,
+  _ctx: ExecutionContext
+) {
+  if (!req.params) {
+    return missingParams();
+  }
+  const db = getDB(env);
+  // get the user
+  const userID = await users.getUserID(db, req.params.user);
+  if (!userID) {
+    return notExists("user");
+  }
+  // Check if the namespace exists
+  const namespaceID = await getNamespaceID(db, userID, req.params.namespace);
+  if (!namespaceID) {
+    return notExists("namespace");
+  }
+  // Delete the namespace
+  const res = await db
+    .prepare("DELETE FROM namespaces WHERE user_id = ? AND name = ?")
+    .bind(userID, req.params.namespace)
+    .run();
+  return Response.json(res);
 }
 
 export default {
-    getNamespaces,
-    createNamespace,
-    deleteNamespace
+  getNamespaces,
+  createNamespace,
+  deleteNamespace,
 };
